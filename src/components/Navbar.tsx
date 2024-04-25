280a7d20972d6b11d50be57690db4bfa8456eb71
jwhubert91 <jwhubert91@gmail.com>
2024-04-24 23:00:09 -0500

    colored text

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
 import React from "react"
-import { RiComputerLine } from "react-icons/ri"
+import { HiOutlineCursorArrowRays } from "react-icons/hi2"
+import { FaArrowRight } from "react-icons/fa"
 
 function Navbar() {
   return (
     <nav className="flex justify-between py-3 px-6 items-center">
       <div className="flex gap-x-2 animated-text">
-        <RiComputerLine size="2em" className="animated-icon" />
+        <HiOutlineCursorArrowRays size="2em" className="animated-text" />
         <h1 className="flex flex-col items-end">
           <span className="text-2xl font-semibold">James Hubert</span>
           <span className="text-xxs">Frontend Web Developer</span>
@@ -18,8 +19,9 @@ function Navbar() {
         <a href="#work" className="hover:text-gray-400">
           Work
         </a>
-        <a href="#contact" className="btn btn-secondary">
+        <a href="#contact" className="btn btn-animated">
           Contact
+          <FaArrowRight />
         </a>
       </div>
     </nav>
