275c4326db4798385bd7e7af41f05bf81cb10ac9
jwhubert91 <jwhubert91@gmail.com>
2024-06-17 18:57:40 -0500

    remove dumb styles from navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,17 +4,17 @@ import { FaPhoneAlt } from "react-icons/fa"
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-center sm:justify-end pr-2 mx-auto w-full fixed z-50">
-      <div className="bg-white p-2 md:mt-1 rounded-full flex gap-x-2">
+    <nav className="flex justify-center sm:justify-end pr-2 w-full">
+      <div className="p-2 md:mt-1 flex gap-x-2">
         <a
-          className="text-gray-800 flex items-center gap-x-1 text-sm mr-3 hover:no-underline hover:text-gray-400"
+          className="text-gray-800 flex items-center gap-x-1 text-sm mr-3 hover:no-underline"
           href="mailto:hello@jameshubert.com"
         >
           <MdEmail />
           hello@jameshubert.com
         </a>
         <a
-          className="text-gray-800 flex items-center gap-x-1 text-sm hover:no-underline hover:text-gray-400"
+          className="text-gray-800 flex items-center gap-x-1 text-sm hover:no-underline"
           href="tel:1-646-653-9881"
         >
           <FaPhoneAlt />
